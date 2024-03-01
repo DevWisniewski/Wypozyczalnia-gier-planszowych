76e831b5590f8d1a7461d0a7bdd3315509f0d845
DevWisniewski <grzegorz.wisniewski.office@gmail.com>
2024-03-01 07:41:36 +0100

    Dodano formularz filtrujący wyświetlane gry

--- games_rental_app/forms.py
+++ games_rental_app/forms.py
@@ -1,16 +1,21 @@
 from django import forms
-from django.utils import timezone
 from django.core.exceptions import ValidationError
 from django.contrib.auth import authenticate, get_user_model
-
 
 User = get_user_model()
 
 class LoginForm(forms.Form):
+    """
+    Formularz logowania dla użytkowników.
+    """
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
     def clean(self):
+        """
+        Waliduje dane logowania.
+        Sprawdza, czy użytkownik o podanej nazwie i haśle istnieje w systemie.
+        """
         cleaned_data = super().clean()
         username = cleaned_data.get('username')
         password = cleaned_data.get('password')
@@ -22,6 +27,10 @@ class LoginForm(forms.Form):
 
 
 class AddUserForm(forms.ModelForm):
+    """
+    Formularz do tworzenia nowego użytkownika.
+    Sprawdza zgodność haseł i zapisuje użytkownika w systemie.
+    """
     password = forms.CharField(widget=forms.PasswordInput)
     password2 = forms.CharField(widget=forms.PasswordInput)
 
@@ -30,16 +39,45 @@ class AddUserForm(forms.ModelForm):
         fields = ['username', 'password', 'password2', 'first_name', 'last_name', 'email']
 
     def clean_password2(self):
+        """
+        Sprawdza, czy oba hasła są identyczne.
+        """
         pass1 = self.cleaned_data.get("password")
         pass2 = self.cleaned_data.get("password2")
         if pass1 and pass2 and pass1 != pass2:
             raise forms.ValidationError("Passwords don't match")
         return pass2
 
-#
     def save(self, commit=True):
+        """
+        Zapisuje użytkownika w systemie, ustawiając hasło.
+        """
         user = super(AddUserForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password"])
         if commit:
             user.save()
         return user
+
+class GameFilterForm(forms.Form):
+    """
+    Formularz służący do filtrowania gier planszowych.
+    """
+
+    # Wybór liczby graczy z opcją '---' na początku, oznaczającą brak wyboru.
+    NUMBER_OF_PLAYERS_CHOICES = [(0, '---')] + [(i, str(i)) for i in range(1, 9)]
+    number_of_players = forms.ChoiceField(choices=NUMBER_OF_PLAYERS_CHOICES, label='Liczba graczy', required=False)
+
+    # Wybór minimalnego wieku gracza, dodano '+' przy liczbie.
+    AGE_CHOICES = [(0, '---')] + [(i, f'{i}+') for i in range(2, 19)]
+    min_age = forms.ChoiceField(choices=AGE_CHOICES, label='Minimalny wiek', required=False)
+
+    # Wybór czasu trwania gry z zakresami czasu w minutach.
+    DURATION_CHOICES = [(0, '---')] + [(i, f'{i} min') for i in [5, 10, 15, 30, 45, 60, 90, 120, 150, 180, 210, 240]]
+    min_duration = forms.ChoiceField(choices=DURATION_CHOICES, label='Minimalny czas gry', required=False)
+    max_duration = forms.ChoiceField(choices=DURATION_CHOICES, label='Maksymalny czas gry', required=False)
+
+    # Wybór zakresu cenowego dla wypożyczenia gier, z etykietami określającymi ceny za dobę.
+    PRICE_CHOICES = [(0, '---')] + [(i, f'{i} zł') for i in [2, 5, 10, 15, 20, 25, 30]]
+    min_price = forms.ChoiceField(choices=PRICE_CHOICES, label='Cena za wypożyczenie od (za dobę)', required=False)
+    max_price = forms.ChoiceField(choices=PRICE_CHOICES, label='do (za dobę)', required=False)
+
--- games_rental_app/views.py
+++ games_rental_app/views.py
@@ -1,17 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views import View
 from django.views.generic import CreateView, FormView, ListView, RedirectView
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth import get_user_model, login, logout
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-from .forms import (LoginForm, AddUserForm,)
+from .forms import LoginForm, AddUserForm, GameFilterForm
 from django.views.generic.detail import DetailView
-from .models import BoardGame
-from django.views.generic import ListView
 from .models import BoardGame
 
 User = get_user_model()
@@ -52,27 +45,89 @@ class MyAccountView(LoginRequiredMixin, TemplateView):
 
 
 class StaticGameDetailsView(TemplateView):
+    """
+    Widok testowy; pokazuje statyczne informacje o grze.
+    """
     template_name = 'games_rental_app/static_game_details.html'
 
-
 class GameDetailsView(DetailView):
+    """
+    Pokazuje szczegółowe informacje o grze na podstawie danych z bazy danych.
+    Używa 'slug' jako identyfikatora do wyszukania gry.
+    """
     model = BoardGame
     template_name = 'games_rental_app/dynamic_game_details.html'
     context_object_name = 'game'
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
 
+class ContactView(TemplateView):
+    """
+    Widok strony kontaktowej. Wyświetla informacje kontaktowe i formularz do kontaktu.
+    """
+    template_name = 'games_rental_app/contact.html'
+
+class WelcomeView(TemplateView):
+    """
+    Widok strony powitalnej. Pokazuje informacje ogólne o serwisie.
+    """
+    template_name = 'games_rental_app/welcome.html'
 
 class GameListView(ListView):
+    """
+    Widok listy gier. Wyświetla gry dostępne w serwisie z możliwością filtrowania wyników.
+    """
     model = BoardGame
     template_name = 'games_rental_app/game_list.html'
     context_object_name = 'games'
 
+    def get_queryset(self):
+        """
+        Pobiera listę gier dostępnych w serwisie, uwzględniając wybory
+        użytkownika w formularzu filtrowania.
+        """
+        # Pobiera domyślną listę wszystkich gier
+        queryset = super().get_queryset()
 
-class ContactView(TemplateView):
-    template_name = 'games_rental_app/contact.html'
+        # Tworzy formularz filtrowania gier na podstawie wyborów użytkownika
+        form = GameFilterForm(self.request.GET)
 
+        # Warunek sprawdzający, czy dane wprowadzone przez użytkownika są zgodnie z regułami formularza
+        if form.is_valid():
+            # Pobiera wartości wybrane przez użytkownika
+            number_of_players = form.cleaned_data.get('number_of_players')
+            min_age = form.cleaned_data.get('min_age')
+            min_duration = form.cleaned_data.get('min_duration')
+            max_duration = form.cleaned_data.get('max_duration')
+            min_price = form.cleaned_data.get('min_price')
+            max_price = form.cleaned_data.get('max_price')
 
-class WelcomeView(TemplateView):
-    template_name = 'games_rental_app/welcome.html'
+            # Filtruje listę gier na podstawie wyborów użytkownika
+            if number_of_players and int(number_of_players) != 0:
+                queryset = queryset.filter(number_of_players__contains=[int(number_of_players)])
+            if min_age:
+                queryset = queryset.filter(minimum_age__gte=min_age)
+            if min_duration and int(min_duration) != 0:
+                queryset = queryset.filter(min_duration__gte=int(min_duration))
+            if max_duration and int(max_duration) != 0:
+                queryset = queryset.filter(max_duration__lte=int(max_duration))
+            if min_price and int(min_price) != 0:
+                queryset = queryset.filter(rental_price_per_day__gte=float(min_price))
+            if max_price and int(max_price) != 0:
+                queryset = queryset.filter(rental_price_per_day__lte=float(max_price))
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        """
+        Pobiera i dostarcza dane kontekstowe dla szablonu.
+        Dodaje formularz filtrowania gier do kontekstu.
+        """
+        # Pobiera domyślne dane kontekstowe
+        context = super().get_context_data(**kwargs)
+
+        # Tworzy formularz filtrowania gier na podstawie danych przekazanych przez użytkownika
+        context['form'] = GameFilterForm(self.request.GET or None)
+
+        return context
 
